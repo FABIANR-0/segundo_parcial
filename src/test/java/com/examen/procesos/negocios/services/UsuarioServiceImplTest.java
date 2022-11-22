4a2df1055661128a9f59806399e393ec8f19a665
KarenM <kamontanon@ufpso.edu.co>
2022-11-21 20:58:46 -0500

    Se agregaron los test para cuando se buscan los usuarios por nombre.

--- src/test/java/com/examen/procesos/negocios/services/UsuarioServiceImplTest.java
+++ src/test/java/com/examen/procesos/negocios/services/UsuarioServiceImplTest.java
@@ -61,4 +61,40 @@ class UsuarioServiceImplTest {
         Assertions.assertEquals(null, usuario1);
 
     }
+    @Test
+    void seDebeEncontrarLosUsuariosPorNombre() {
+        //Given
+        Usuario usuario = new Usuario();
+        usuario.setId(1L);
+        usuario.setNombre("pepe");
+        usuario.setApellidos("Rios");
+        usuario.setFechaNacimiento(new Date(2004, 7, 14));
+        usuario.setCorreo("pepe@gmail.com");
+        usuario.setPassword("1234");
+        usuario.setDireccion("kdx.010-310");
+        usuario.setDocumento("1064836389");
+        usuario.setTelefono("3144454761");
+
+        //when
+        when(usuarioRepository.findAllByNombre("pepe")).thenReturn(List.of(usuario));
+
+
+        ResponseEntity<List<Usuario>> usuario1 = usuarioServiceImpl.allUsersByName("pepe");
+
+        //then
+        Assertions.assertNotNull(usuario1);
+    }
+    @Test
+    void whenNoEncuentraUsuariosPorNombre() {
+        Usuario usuario = null;
+
+        //When
+        when(usuarioRepository.findAllByNombre("pepe")).thenReturn(Collections.emptyList());
+
+        List<Usuario> usuario1 = usuarioServiceImpl.allUsersByName("pepe").getBody();
+
+        //Then
+        Assertions.assertEquals(null, usuario1);
+
+ }
 }
